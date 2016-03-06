8856963b38d1563d9b96689c836ba02ddfbcbdcf
Seamus <seamusstephens@gmail.com>
2016-03-06 15:17:14 +1000

    update

--- index.js
+++ index.js
@@ -1,44 +1,11 @@
-var express = require('express');
-var app = express();
-var io = require('socket.io')(app.listen(8081));
-// var socket = io();
+var server = require("./server");
+var router = require("./route");
+var requestHandlers = require("./requestHandlers");
 
-var five = require('johnny-five')
-    , led
-    , toggleState = false;
+var debug = false;
 
-app.use(express.static(__dirname + '/app'));
+var handle = {}
+handle["/"] = requestHandlers.sendInterface;
+handle["/interface"] = requestHandlers.sendInterface;
 
-app.get('/', function (res) {
-  	res.sendfile('/index.html');
-});
-
-var board = new five.Board({
-  	repl:false,
-    port:"COM3"
-});
-
-
-board.on('ready', function () {
-    var commands;
-    var led = new five.Led(13);
-    
-    commands = null;
-
-    io.on('connection', function (socket) {
-        socket.on('toggled', function () {
-            toggleLED(led);
-        });
-    });
-});
-
-
-function toggleLED(led){
-    io.emit('toggled');
-    toggleState = !toggleState;
-    
-    if (toggleState) led.on();
-    else led.off();
-}
-
-document.getElementById('toggled').onclick = toggleLED;
+server.start(router.route,handle,debug);
